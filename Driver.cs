2eb4f401c2c49d8a5e3e708b6ffc3004b40172d3
djrmlrfs <542609160@qq.com>
2018-08-21 22:11:43 +0800

    z

---
+++ Driver.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+using System;
+
+namespace Quantum.Shor
+{
+    class Driver
+    {
+        static int gcd(int a, int b)
+        {
+            if (b == 0) return a;
+            else return gcd(b,a%b);
+        }
+        static int pow(int a, int r, int N)
+        {
+            int rr = r, ans = 1, tmp = a;
+            while (rr > 0)
+            {
+                if (rr % 2 == 1) ans = ans*tmp%N;
+                tmp = tmp*tmp%N;    rr = rr/2;
+            }
+            return ans;
+        }
+        static int Orderf(int x, int N, int t, int L)
+        {
+            using (var sim = new QuantumSimulator())
+            {
+                int cnt = 0;
+                int[] frac = new int[2*t];
+                var tmp = QOF.Run(sim,x,N,t,L).Result;
+                while(true)
+                {
+                    tmp = 1.0/tmp;
+                    frac[cnt] = (int)Math.Floor(tmp);
+                    tmp = tmp-frac[cnt++];
+                    if (Math.Abs(tmp) < 1e-6) break;                
+                }
+                int nmr = 0, dnm = 1, temp;
+                for (int i = cnt-1; i >= 0; i--)
+                {
+                    temp = nmr+dnm*frac[i];
+                    nmr = dnm;  dnm = temp;
+                }
+                return dnm;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Random ran = new Random();
+            bool[] fail = new bool[128];
+            int[] need = new int[2];
+            need[0] = 15;   need[1] = 21;
+            for (int zz = 0; zz < 2; ++zz)
+            {
+                N = need[zz];
+                for (int i = 2; i < N; ++i) fail[i] = false;
+                int tot = 0;
+                Console.WriteLine("Factorize "+N+":");
+                while (tot+2 < N)
+                {
+                    int x = ran.Next(2,N);
+                    if (fail[x]) continue;
+                    ++tot;  fail[x] = true;
+                    if (gcd(N,x) != 1)
+                    {
+                        Console.WriteLine("rand a factor:"+x+" in "+tot+" times");
+                        continue;
+                    }
+                    int r = Orderf(x,N,5,7);
+                    if (r >= N)
+                    {
+                        --tot;
+                        fail[x] = false;
+                        continue;
+                    }
+                    int c = pow(x,r,N);
+                    if (c != 1)
+                    {
+                        tot--;
+                        fail[x] = false;
+                        continue;
+                    }
+                    if (r%2 != 0) continue;
+                    if ((pow(x,r/2,N)+1)%N == 0)    continue;
+
+                    int p = gcd(pow(x,r/2,N)-1,N);
+                    int q = gcd(pow(x,r/2,N)+1,N);
+                    Console.WriteLine(N+" = "+p+" * "+q);
+                    break;
+                }
+                Console.WriteLine("We tried "+tot+" times\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Operation.qs
@@ -0,0 +1,136 @@
+﻿namespace Quantum.Shor
+{
+    open Microsoft.Quantum.Primitive;
+    open Microsoft.Quantum.Canon;
+	operation Set(desired:Result, q1:Qubit):()
+    {
+        body
+        {
+            let current = M(q1);
+            if (desired != current)
+            {
+                X(q1);
+            }
+        }
+    }
+    operation Ux(x:Int, N:Int, y:Qubit[]):()
+	{
+		body
+		{
+			let TMP = LittleEndian(y);
+			ModularMultiplyByConstantLE(x,N,TMP);
+		}
+		controlled auto
+	}
+	operation QFT(q:Qubit[]):()
+    {
+        body
+        {
+			mutable n = Length(q);
+			mutable pow = new Int [n+1];
+			set pow[0] = 1;
+			for (i in 1 .. n)
+			{
+				set pow[i] = 2*pow[i-1]; 
+			}
+            for (i in 0 .. n-1)
+			{
+				H(q[i]);
+				for (j in i+1 .. n-1)
+				{
+					(controlled R1)([q[j]],(2*PI()/pow[j-i+1],q[i));
+				}
+			}
+			for (i in 0 .. (n-1)/2)
+			{
+				SWAP(q[i],q[n-1-i]);
+			}
+        }
+		adjoint
+		{
+			mutable n = Length(q);
+			mutable pow = new Int [n+1];
+			set pow[0] = 1;
+			for (i in 1 .. n)
+			{
+				set pow[i] = 2 * pow[i-1]; 
+			}
+			for (i in 0 .. (n / 2 - 1))
+			{
+				SWAP(q[i], q[n-1-i]);
+			}
+			for (i in n-1 .. -1 .. 0)
+			{
+				for (j in n-1 .. -1 .. i+1)
+				{
+					(controlled R1)([q[j]], (-2*PI()/pow[j-i+1],q[i]));
+				}
+				H(q[i]);
+			}
+		}
+	}
+	operation PE(x:Int, N:Int, reg2:Qubit[], t:Int, L:Int):(Int[])
+	{
+		body
+		{
+			mutable rtn = new Int[t];
+			using (reg1 = Qubit[t]) 
+			{
+				for (i in 0 .. t-1)
+				{
+					Set(Zero, reg1[i]);
+					H(reg1[i]);
+				}
+				mutable z = x;
+				for (i in 0 .. t-1) 
+				{
+					(Controlled Ux) ([reg1[t - i - 1]], (z, N, reg2));
+					set z = z*z%N;
+				}
+				(Adjoint QFT) (reg1);
+
+				for(i in 0 .. t - 1)
+				{
+					let result = M(reg1[i]);
+					if(result == Zero)
+					{
+						set rtn[i] = 0;
+					}
+					if(result == One)
+					{
+						set rtn[i] = 1;
+					}
+				}
+				ResetAll(reg1);
+			}
+			return rtn;
+		}
+	}
+	operation QOF(x:Int, N:Int, t:Int, L:Int):(Double)
+	{
+		body
+		{
+			mutable phi = 0.0;
+			mutable tw = 0.5;
+			using (reg2 = Qubit[L])
+			{
+				for (i in 1 .. L - 1)
+				{
+					Set(Zero,reg2[i]);
+				}
+				Set(One, reg2[0]);
+				let out = PE(x,N,reg2,t,L);
+				for (i in 0 .. t - 1)
+				{
+					if (out[i] == 1)
+					{
+						set phi = phi+tw;
+					}
+					set tw = tw*0.5;
+				}
+				ResetAll(reg2);
+			}
+			return phi;
+		}
+	}
+}
